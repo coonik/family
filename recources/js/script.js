e976b1edce72085bc0639a560a8356b9ff097b64
coonik <vuonglongxungba@gmail.com>
2020-11-02 23:43:19 +0700

    fix nav-mobile

--- recources/js/script.js
+++ recources/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
   const icon = $('#nav-menu');
   icon.click(() => {
-    $('.nav-mobile ul').slideToggle(200);
+    $('.nav-list ul').slideToggle(200);
     if (icon.attr('name') === 'menu-outline') {
       icon.attr('name', 'close-outline');
     } else {
